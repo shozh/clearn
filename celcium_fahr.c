d72625759f4bee2adfb36ee5b198c545d6dcb674
qoade <as8art@gmail.com>
2023-03-08 16:30:32 +0300

    fahr-cels done

--- celcium_fahr.c
+++ celcium_fahr.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
 
 float fahr(float c) {
-    return c * 9 / 5 + 32
+    return c * 9 / 5 + 32;
 }
 
 float cel(float f) {
-    return (f - 32) * 5 / 9
+    return (f - 32) * 5 / 9;
 }
 
 int main(void) {
 
-    printf("What's the scale? Fahrenheit/Celsius");
-    scanf("")
-    char[100] s;
-
+    printf("What's the scale? Fahrenheit/Celsius\n");
+    char c = getchar();
+    float x;
+    printf("Input the value: ");
+    scanf("%f", &x);
+    if (c == 'F') {
+        printf("%.3f", cel(x));
+    } else if (c == 'C') {
+        printf("%.3f", fahr(x));
+    } else {
+        printf("\nWhat the hell is it?");
+        return 404;
+    }
     return 0;
 }
