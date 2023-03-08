fef8d302ac412a78b300528ad2212222cd8d8438
qoade <as8art@gmail.com>
2023-03-08 16:17:15 +0300

    First commiting: create git files

---
+++ celcium_fahr.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+float fahr(float c) {
+    return c * 9 / 5 + 32
+}
+
+float cel(float f) {
+    return (f - 32) * 5 / 9
+}
+
+int main(void) {
+
+    printf("What's the scale? Fahrenheit/Celsius");
+    scanf("")
+    char[100] s;
+
+    return 0;
+}
